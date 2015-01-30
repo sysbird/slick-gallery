d5a37b43bc177f1205c92b674a5fae847302ab0d
sysbird <koro@gmail.com>
2015-01-30 17:03:37 +0900

    hello plugin!

---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Swipe Gallery ===
+Contributors: sysbird
+Plugin URI: http://wordpress.org/plugins/swipe-gallery/
+Tags: shortcode, slider, swipe, image
+Requires at least: 3.8
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Mobile touch slider with recent posts featured-image.
+
+
+== Description ==
+Mobile touch slider with recent posts featured-image.
+
+= Features =
+* The plugin displays a slider by shortcode.  
+* The slider displays recent posts featured-image.  
+* Images on the slider link to the post page.  
+* It's compatible with responsive web design and mobile touch.   
+* [Swiper](http://www.idangero.us/sliders/swiper/) the jQuery plugin is licensed under GPL & MIT.  
+
+= Usage =
+1. Set featured-image in post.  
+2. Please write shortcode [featured-images-swiper] in the template.  
+3. You can set number of thumbnail the slider.  
+   example. [featured-images-swiper number="10"]  
+   number: umber of thumbnail the slider(default:10)  
+
+[Demo](http://www.sysbird.jp/birdsite/2007/09/21/20070921-hakodate/)  
+
+= Contributors =
+TORIYAMA Yuko at [sysbird](http://www.sysbird.jp/wptips/)  
+
+= More Information =
+[Description in Japanese](http://www.sysbird.jp/wptips/featured-images-swiper/)  
+
+
+== Installation ==
+1. Upload the "Featured Images Swiper" folder to the plugins directory in your WordPress installation.  
+2. Go to plugins list and activate "Featured Images Swiper".  
+
+
+== Screenshots ==
+1. The swipe slider with recent posts featured-image.
+
+
+== Changelog ==
+= 1.0 =
+* Hello, world!  
---
+++ slick-gallery.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ Plugin Name: Slick Gallery
+ Plugin URI: http://wordpress.org/plugins/slick-gallery/
+ Description: Mobile touch slider with recent posts featured-image.
+ Author: sysbird
+ Author URI: http://www.sysbird.jp/wptips
+ Version: 1.0
+ License: GPLv2 or later
+*/
+
+//////////////////////////////////////////////////////
+// Wordpress 3.6+
+global $wp_version;
+if ( version_compare( $wp_version, "3.8", "<" ) ){
+	return false;
+}
+
+//////////////////////////////////////////////////////
+// Start the plugin
+class SlickGallery {
+
+	//////////////////////////////////////////
+	// construct
+	function __construct() {
+		add_shortcode('slick-gallery', array( &$this, 'shortcode' ) );
+		add_action( 'wp_enqueue_scripts', array( &$this, 'add_script' ) );
+		add_action( 'wp_print_styles', array( &$this, 'add_style' ) );
+	}
+
+	//////////////////////////////////////////
+	// add JavaScript
+	function add_script() {
+		wp_enqueue_script( 'jquery-masonry' );
+
+		$filename = plugins_url( dirname( '/' .plugin_basename( __FILE__ ) ) ).'/js/slick-gallery.js';
+		wp_enqueue_script( 'slick-gallery', $filename, array( 'jquery' ), '1.0' );
+	}
+
+	//////////////////////////////////////////
+	// add css
+	function add_style() {
+		$filename = plugins_url( dirname( '/' .plugin_basename( __FILE__ ) ) ).'/css/slick-gallery.css';
+		wp_enqueue_style( 'slick-gallery', $filename, false, '1.0' );
+	}
+
+	//////////////////////////////////////////
+	// ShoetCode
+	function shortcode( $atts ) {
+
+		global $post;
+
+		$output = '';
+		$args = array( 'post_type'       => 'attachment',
+						'posts_per_page' => -1,
+						'post_parent'    => $post->ID,
+						'post_mime_type' => 'image',
+						'orderby'        => 'menu_order',
+						'order'          => 'ASC' );
+		$images = get_posts( $args );
+		if ( $images ) {
+			foreach( $images as $image ){
+				$src = wp_get_attachment_url( $image->ID );
+				$thumbnail = wp_get_attachment_image_src($image->ID, 'thumbnail');
+				$file = get_attached_file( $image->ID );
+				$output .= '<div class="item"><a href="' .$src .'"><img src="' .$thumbnail[0] .'" alt="' .$image->post_title .'"></a></div>';
+  			}
+		}
+
+		if( !empty( $output ) ){
+			$output = '<div class="slick-gallery">' .$output .'</div>';
+		}
+
+		return $output;
+	}
+}
+$SlickGallery = new SlickGallery();
+?>
